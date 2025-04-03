6cb5feabd6f54dcd209f6328f710ce1465277ff0
TungVLe <tunle0801@gmail.com>
2025-04-03 16:30:36 -0400

    reorganize codebase for html

--- frontend/src/lib/eventhub/index.ts
+++
@@ -1,4 +0,0 @@
-import { EventHub } from "@/lib/eventhub/EventHub";
-import { Events } from "@/lib/eventhub/Events";
-
-export { EventHub, Events };
--- frontend/src/lib/models/user.ts
+++
@@ -1,6 +0,0 @@
-export interface User {
-  email: string;
-  name: string;
-}
-
-// For form validation I recommend looking into Zod
