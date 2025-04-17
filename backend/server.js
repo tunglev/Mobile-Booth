a9a31d202c6c51c226af60f570534412a37b9543
Mihir Nagarkatti <mihir@tensordock.com>
2025-04-17 16:29:46 -0400

    fixed frontend pathname

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from the "client" directory
-app.use(express.static(path.join(__dirname, '../client')));
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 // Fallback to serve `index.html` for any unmatched routes (useful for SPAs)
 // Replace the problematic catchall route
 app.use((req, res) => {
-    res.sendFile(path.join(__dirname, '../client/index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
 // Start the server
